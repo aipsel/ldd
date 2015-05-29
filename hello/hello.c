caa89364d8dafd63529261692b8dfcd2c3410149
aipsel <875384031@qq.com>
2015-05-29 22:26:55 +0800

    add two parameters to hello world module.
    The two parameters are whom and howmany.
    To load this module, for example, use:
        insmod hello.ko whom=everyone howmany=5

--- hello/hello.c
+++ hello/hello.c
@@ -1,11 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
 MODULE_AUTHOR("Aipsel");
 MODULE_LICENSE("Dual BSD/GPL");
 
+static char *whom = "world";
+static int howmany = 1;
+
+module_param(whom, charp, S_IRUGO);
+module_param(howmany, int, S_IRUGO);
+
 static int hello_init(void) {
-    printk(KERN_ALERT "Hello, world.\n");
+    while (howmany-- > 0)
+        printk(KERN_ALERT "Hello, %s.\n", whom);
     return 0;
 }
 
